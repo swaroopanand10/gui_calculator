3cf182b218616b0d616868c8256725b95bb9aced
swaroopanand <swaroopanand695@gmail.com>
2024-02-05 20:51:30 +0530

    added javascript file

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+const keypad_elements_input = Array.from(document.querySelectorAll('.keypad-elements.input'))
 
+for (let element = 0; element < keypad_elements.length; element++) {
+  keypad_elements[element].addEventListener('click', visible_capture)
+}
+
+function visible_capture(e) {
+  alert(e.target.innerText)
+}
